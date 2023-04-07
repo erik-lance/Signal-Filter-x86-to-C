77fecad96fcf4df2b265f8a83ad2a6381f43fa9d
erik-lance <eriklance@gmail.com>
2023-04-07 13:08:05 +0800

    Added inputs

--- tionEmp3.asm
+++ tionEmp3.asm
@@ -1,19 +1,33 @@
 ; Erik Lance L. Tiongquico - S14
 global main
-extern printf
+extern printf, system, scanf
 
 section .data
-promptstart db "The signal has %d samples. ",0
+clrstr db "cls",0
+promptstart db "The signal has %d samples. ",10,0
 prompt db "Enter coefficient h%d:",0
 promptend db "Filter output:",0
 promptagain db "Want to try again? (y/n): ",0
 
-signal dd -1, 3, 4, 0, 9, 0x80000000
+scanfmtD db "%d",0
 
+signal dd -1, 3, 4, 0, 9, 0x80000000
 sample_count dd 0
+
+hnum dd 0
+h0 dd 0
+h1 dd 0
+h2 dd 0
+
+
 
 section .text
 main:
+    ;cls
+    push clrstr
+    call system
+    add  esp, 4
+
     mov ebp, esp; for correct debugging
     xor EAX, EAX    ; accumulator
     xor EBX, EBX    ; pointer for data
@@ -38,9 +52,52 @@ begin:
     call printf
     add esp, 8  ; 2 bytes pushed
     
+    xor ECX, ECX
+    mov [hnum], ECX
     
+    ; --- User inputs 1
+    push dword [hnum]
+    push prompt
+    call printf
+    add esp, 8
     
+    push h0
+    push scanfmtD
+    call scanf
+    add esp, 8
     
+    ; --- User inputs 2
+    inc ECX
+    mov [hnum], ECX
+    
+    push dword [hnum]
+    push prompt
+    call printf
+    add esp, 8
+    
+    push h1
+    push scanfmtD
+    call scanf
+    add esp, 8
+    
+    ; --- User inputs 3
+    inc ECX
+    mov [hnum], ECX
+    
+    push dword [hnum]
+    push prompt
+    call printf
+    add esp, 8
+    
+    push h2
+    push scanfmtD
+    call scanf
+    add esp, 8
+
+
+operate:
+
+print_output:
     
     
     xor eax, eax
