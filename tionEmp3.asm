117b96fb23e597a56d220475931894958010c669
erik-lance <eriklance@gmail.com>
2023-04-07 19:19:59 +0800

    Fixed coefficient bug with register, used EBX instead

--- tionEmp3.asm
+++ tionEmp3.asm
@@ -5,7 +5,7 @@ extern printf, system, scanf
 section .data
 clrstr db "cls",0
 promptstart db "The signal has %d samples. ",10,0
-prompt db "Enter coefficient h%d:",0
+prompt db "Enter coefficient h%d:",10,0
 promptend db "Filter output:",10,0
 promptagain db "Want to try again? (y/n): ",0
 
@@ -13,8 +13,8 @@ scanfmtD db "%d",0
 
 signal dd -1, 3, 4, 0, 9, 0x80000000
 sample_count dd 0
+signal_end dd 0
 
-hnum dd 0
 h0 dd 0
 h1 dd 0
 h2 dd 0
@@ -29,6 +29,7 @@ z2 dd 0
 
 section .text
 main:
+    mov ebp, esp; for correct debugging
     ;cls
     push clrstr
     call system
@@ -52,50 +53,48 @@ count_samples:
     mov [sample_count], ECX
 
 begin:
+    mov [signal_end], EBX
+    
     ; printf(promptstart)
     push dword [sample_count]
     push promptstart
     call printf
     add esp, 8  ; 2 bytes pushed
     
-    xor ECX, ECX
-    mov [hnum], ECX
-    
     ; --- User inputs 1
-    push dword [hnum]
+    xor EBX, EBX
+    push EBX
     push prompt
     call printf
     add esp, 8
     
-    push h0
+    push h0 ; Scan to h0
     push scanfmtD
     call scanf
     add esp, 8
     
     ; --- User inputs 2
-    inc ECX
-    mov [hnum], ECX
+    inc EBX
     
-    push dword [hnum]
+    push EBX
     push prompt
     call printf
     add esp, 8
     
-    push h1
+    push h1 ; Scan to h1
     push scanfmtD
     call scanf
     add esp, 8
     
     ; --- User inputs 3
-    inc ECX
-    mov [hnum], ECX
+    inc EBX
     
-    push dword [hnum]
+    push EBX
     push prompt
     call printf
     add esp, 8
     
-    push h2
+    push h2 ; Scan to h2
     push scanfmtD
     call scanf
     add esp, 8
@@ -103,14 +102,12 @@ begin:
 
 operate:
     ; EBX contains pointer to last number
+    mov EBX, [signal_end]
     add EBX, -4
     
     ; Store bottom pointer of the list
     xor EDX, EDX
-    
-    mov ESI, EDX    ; Copy
-    
-    xor EDI, EDI    ; For counting loop
+    xor ESI, ESI    ; For counting loop
 
     push promptend
     call printf
@@ -127,9 +124,9 @@ comma:
 filter:
     ; --- z1
     xor EAX, EAX
-    add EAX, [h2]   
+    add EAX, dword [h2]   
     
-    mov ECX, [signal+EDX]   ; Get x0
+    mov ECX, dword [signal+EDX]   ; Get x0
     mul EAX
     
     mov [z1], ECX   ; x(n) * h2
@@ -137,12 +134,12 @@ filter:
     
     ; --- z2
     xor EAX, EAX
-    add EAX, [h1]
+    add EAX, dword [h1]
     
     mov ECX, [signal+EDX]   ; Get x1
     mul EAX
     
-    mov [z2], ECX   ; x(n+1) * h1
+    mov dword [z2], ECX   ; x(n+1) * h1
     add EDX, 4
     
     ; --- FINAL
@@ -152,8 +149,8 @@ filter:
     mov ECX, [signal+EDX]   ; Get x2
     mul EAX
     
-    add EAX, [z1]   ; [ x(n+0) * h2 ] +
-    add EAX, [z2]   ; [ x(n+1) * h1 ] +  [x(n+2) * h0 ]
+    add EAX, dword [z1]   ; [ x(n+0) * h2 ] +
+    add EAX, dword [z2]   ; [ x(n+1) * h1 ] +  [x(n+2) * h0 ]
     
     
 print_output:
@@ -165,13 +162,13 @@ print_output:
     call printf
     add esp, 8
     
-    add EDI, 3  ; Add 1 for loop iteration, and 2 to make sure below last 2
-    cmp EDI, [sample_count]  ; Check if reached last 2
+    add ESI, 3  ; Add 1 for loop iteration, and 2 to make sure below last 2
+    cmp ESI, [sample_count]  ; Check if reached last 2
     je tapos  ; Program is done!
     
-    add EDI, -2     ; Normalize back to normal looping
+    add ESI, -2     ; Normalize back to normal looping
 
-    mov EDX, EDI    ; Start at index of loop
+    mov EDX, ESI    ; Start at index of loop
     imul EDX, 4     ; Pointer format
     jmp comma   ; Back to filtering (and add comma to number!)
     
